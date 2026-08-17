0ebe94a913e20b10f0769205ab21761c267e6120
Sadhana Katabatthini <164516875+Sadhanakatabatthini@users.noreply.github.com>
2026-08-17 20:14:04 +0530

    conditional statements - If and if else

---
+++ DAY-10/if.py
@@ -0,0 +1,15 @@
+'''
+sales = int(input("Enter Sales: "))
+
+if sales>1000:
+    print("Best Seller")
+
+eli_acc=eval(input("Enter Eligible Account:"))
+ver_sub=eval(input("Enter Meta Verified Subscription:"))
+
+if eli_acc and ver_sub:
+    print("Verified Badge granted")
+'''
+rain_status=eval(input("enter the rain status:"))
+if rain_status:
+    print("Extra rain charges applied")
